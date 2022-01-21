975317b57b47a4a9a515ec8f65b93c36ae3bbb84
oocheol <oocheol@naver.com>
2022-01-21 20:16:07 +0900

    init

--- Java_2/src/ex02.java
+++ Java_2/src/ex02.java
@@ -2,5 +2,24 @@
 
 public class ex02 {
 
+	public static void main(String[] args) {
+	
+		// 1. 논리형 - boolean
+		boolean name1 = true ;
+		
+		// 2-1. 문자형
+		char name2 = 'a';
+		
+		// 2-2. 레퍼런스형
+		String name7 = "abc";
+		
+		// 3. 정수형
+		int name3 = 8;
+		
+		// 4. 실수형
+		double name4 = 3.14;
+		float name5 = 1.5f;
+		
+	}
 
 }
